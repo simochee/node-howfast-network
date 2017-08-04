d9c4c39b4a94cc8dc17a4a728ae098b436d94a9c
Ryoya Tamura <simochee@gmail.com>
2017-08-04 21:41:04 +0900

    ちょっと親切になった

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const notifier = require('node-notifier');
 const speedTest = require('speedtest-net');
+const path = require('path');
+const log = require('debug')('howfast:log');
 const error = require('debug')('howfast:error');
 const success = require('debug')('howfast:success');
 
@@ -7,11 +9,13 @@ const test = speedTest({
     maxTime: 5000,
 });
 
+log('取得しています。しばらくお待ちください...');
+
 test.on('data', (data) => {
     notifier.notify({
         title: 'ネットワークスピードを取得しました',
         message: `Upload: ${data.speeds.upload}Mbps, Download: ${data.speeds.download}Mbps`,
-        icon: `${__dirname}/icon.png`,
+        icon: path.join(__dirname, 'icon.png'),
         sound: true,
     });
     success(`Upload: ${data.speeds.upload}Mbps, Download: ${data.speeds.download}Mbps`);
@@ -21,7 +25,7 @@ test.on('error', (err) => {
     notifier.notify({
         title: 'ネットワークスピードが取得できませんでした',
         message: err,
-        icon: `${__dirname}/icon.png`,
+        icon: path.join(__dirname, 'icon.png'),
         sound: true,
     });
     error(JSON.stringify(err, null, '    '));
